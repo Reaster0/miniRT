e355c352fefb3bbb56146b2d5662b7b308bc7b95
reaster <earnaud@student.42.fr>
2021-03-15 21:58:57 +0100

    weird transparence glitch on square but i will debug it later

--- srcs/squares.c
+++ srcs/squares.c
@@ -6,7 +6,7 @@
 /*   By: earnaud <earnaud@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/17 20:55:28 by earnaud           #+#    #+#             */
-/*   Updated: 2021/03/15 20:36:14 by earnaud          ###   ########.fr       */
+/*   Updated: 2021/03/15 21:51:21 by earnaud          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,33 +23,76 @@ t_triangle new_triangle(t_3d a, t_3d b, t_3d c, int color)
 	return (triangle);
 }
 
+static int in_it(t_ray *ray, t_square *square, float *t)
+{
+	float dDotN;
+
+	if (dot_product(ray->endpoint, square->orient) > 0.f)
+		square->orient = multiply_v(-1.f, square->orient);
+	dDotN = dot_product(ray->endpoint, square->orient);
+	if (dDotN == 0.f)
+		return (0);
+	*t = dot_product(sub_product(square->center, ray->startpoint), square->orient) / dDotN;
+	if (*t <= 0.000001f || *t >= ray->t)
+		return (0);
+	return (1);
+}
+
+int square_len(t_square *square, t_3d interp)
+{
+	t_3d temp;
+	if (interp.x < 0.f)
+		interp.x *= -1.f;
+	if (interp.y < 0.f)
+		interp.y *= -1.f;
+	temp = sub_product(interp, square->center);
+
+	if (temp.x > square->side / 2 || temp.y > square->side / 2)
+		return (0);
+
+	return (1);
+}
+
 int inter_square(t_ray *ray, t_square *square)
 {
-	int result;
-	t_3d point[4];
-	point[0].x = square->center.x - square->side / 2;
-	point[0].y = square->center.y - square->side / 2;
-	point[1].x = square->center.x + square->side / 2;
-	point[1].y = square->center.y - square->side / 2;
-	point[2].x = square->center.x + square->side / 2;
-	point[2].y = square->center.y + square->side / 2;
-	point[3].x = square->center.x - square->side / 2;
-	point[3].y = square->center.y + square->side / 2;
+	float t;
+	t_3d interp;
 
-	t_triangle triangle1;
-	t_triangle triangle2;
-	result = 0;
-	triangle1 = new_triangle(point[0], point[1], point[2], square->color);
-	triangle2 = new_triangle(point[0], point[2], point[3], square->color);
-	if (inter_triangle2(ray, &triangle1))
-		result = (1);
-	if (inter_triangle2(ray, &triangle2))
-		result = (1);
-	if (result == 1)
-	{
-		ray->shape_point = sub_product(point[2], divide_vr(2, point[0]));
-		return (result);
-	}
+	if (!in_it(ray, square, &t))
+		return (0);
+	interp = add_product(multiply_v(t, ray->endpoint), ray->startpoint);
+	if (!square_len(square, interp))
+		return (0);
+	ray->shape_color = square->color;
+	ray->t = t;
+	ray->shape_normale = square->orient;
+	return (1);
+
+	// int result;
+	// t_3d point[4];
+	// point[0].x = square->center.x - square->side / 2;
+	// point[0].y = square->center.y - square->side / 2;
+	// point[1].x = square->center.x + square->side / 2;
+	// point[1].y = square->center.y - square->side / 2;
+	// point[2].x = square->center.x + square->side / 2;
+	// point[2].y = square->center.y + square->side / 2;
+	// point[3].x = square->center.x - square->side / 2;
+	// point[3].y = square->center.y + square->side / 2;
+
+	// t_triangle triangle1;
+	// t_triangle triangle2;
+	// result = 0;
+	// triangle1 = new_triangle(point[0], point[1], point[2], square->color);
+	// triangle2 = new_triangle(point[0], point[2], point[3], square->color);
+	// if (inter_triangle2(ray, &triangle1))
+	// 	result = (1);
+	// if (inter_triangle2(ray, &triangle2))
+	// 	result = (1);
+	// if (result == 1)
+	// {
+	// 	ray->shape_point = sub_product(point[2], divide_vr(2, point[0]));
+	// 	return (result);
+	// }
 }
 
 int inter_squares(t_ray *ray, t_square *square)
--- srcs/triangles.c
+++ srcs/triangles.c
@@ -6,7 +6,7 @@
 /*   By: earnaud <earnaud@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/17 20:53:13 by earnaud           #+#    #+#             */
-/*   Updated: 2021/03/15 20:36:21 by earnaud          ###   ########.fr       */
+/*   Updated: 2021/03/15 21:31:08 by earnaud          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,10 @@
 
 //problemes sur les triangles qui sont derrieres la camera
 
-int in_it(t_ray *ray, t_triangle *triangle, float *t, t_3d *normal)
+static int in_it(t_ray *ray, t_triangle *triangle, float *t, t_3d *normal)
 {
 	t_3d edge1;
 	t_3d edge2;
-	//t_3d normal;
 	float dDotN;
 
 	edge1 = sub_product(triangle->b, triangle->a);
@@ -63,55 +62,55 @@ int inter_triangle2(t_ray *ray, t_triangle *triangle)
 	return (1);
 }
 
-int inter_triangle(t_ray *ray, t_triangle *triangle)
-{
-	t_3d edge1;
-	t_3d edge2;
-	t_3d orig_minus_vert0;
-	t_3d cross_raydir_edge2;
-	t_3d cross_oriminusvert0_edge1;
-	float determinent, inv_determinent;
-	float t;
+// int inter_triangle(t_ray *ray, t_triangle *triangle)
+// {
+// 	t_3d edge1;
+// 	t_3d edge2;
+// 	t_3d orig_minus_vert0;
+// 	t_3d cross_raydir_edge2;
+// 	t_3d cross_oriminusvert0_edge1;
+// 	float determinent, inv_determinent;
+// 	float t;
 
-	edge1 = sub_product(triangle->b, triangle->a);
-	edge2 = sub_product(triangle->c, triangle->a);
+// 	edge1 = sub_product(triangle->b, triangle->a);
+// 	edge2 = sub_product(triangle->c, triangle->a);
 
-	cross_raydir_edge2 = cross_product(ray->endpoint, edge2);
-	determinent = dot_product(edge1, cross_raydir_edge2);
+// 	cross_raydir_edge2 = cross_product(ray->endpoint, edge2);
+// 	determinent = dot_product(edge1, cross_raydir_edge2);
 
-	if (determinent > -0.000001 && determinent < 0.000001)
-		return (0);
+// 	if (determinent > -0.000001 && determinent < 0.000001)
+// 		return (0);
 
-	inv_determinent = 1.f / determinent;
+// 	inv_determinent = 1.f / determinent;
 
-	orig_minus_vert0 = sub_product(ray->startpoint, triangle->a);
+// 	orig_minus_vert0 = sub_product(ray->startpoint, triangle->a);
 
-	triangle->bary.y = dot_product(orig_minus_vert0, cross_raydir_edge2) * inv_determinent;
-	if (triangle->bary.y < 0.0f || triangle->bary.y > 1.0f)
-		return (0);
+// 	triangle->bary.y = dot_product(orig_minus_vert0, cross_raydir_edge2) * inv_determinent;
+// 	if (triangle->bary.y < 0.0f || triangle->bary.y > 1.0f)
+// 		return (0);
 
-	cross_oriminusvert0_edge1 = cross_product(orig_minus_vert0, edge1);
+// 	cross_oriminusvert0_edge1 = cross_product(orig_minus_vert0, edge1);
 
-	triangle->bary.z = dot_product(ray->endpoint, cross_oriminusvert0_edge1) * inv_determinent;
-	if (triangle->bary.z < 0.0f || triangle->bary.z > 1.0f)
-		return (0);
-	triangle->bary.x = 1 - triangle->bary.y - triangle->bary.z;
+// 	triangle->bary.z = dot_product(ray->endpoint, cross_oriminusvert0_edge1) * inv_determinent;
+// 	if (triangle->bary.z < 0.0f || triangle->bary.z > 1.0f)
+// 		return (0);
+// 	triangle->bary.x = 1 - triangle->bary.y - triangle->bary.z;
 
-	if (triangle->bary.x < 0.0f || triangle->bary.z > 1.0f)
-		return (0);
+// 	if (triangle->bary.x < 0.0f || triangle->bary.z > 1.0f)
+// 		return (0);
 
-	t = dot_product(edge2, cross_oriminusvert0_edge1) * inv_determinent;
-	if (t < ray->t)
-	{
-		ray->shape_normale = cross_product(sub_product(triangle->b, triangle->a), sub_product(triangle->c, triangle->a));
-		ray->shape_point = new_3d((triangle->a.x + triangle->b.x + triangle->c.x) / 2, (triangle->a.y + triangle->b.y + triangle->c.y) / 2, (triangle->a.z + triangle->b.z + triangle->c.z) / 2);
-		ray->t = t;
+// 	t = dot_product(edge2, cross_oriminusvert0_edge1) * inv_determinent;
+// 	if (t < ray->t)
+// 	{
+// 		ray->shape_normale = cross_product(sub_product(triangle->b, triangle->a), sub_product(triangle->c, triangle->a));
+// 		ray->shape_point = new_3d((triangle->a.x + triangle->b.x + triangle->c.x) / 2, (triangle->a.y + triangle->b.y + triangle->c.y) / 2, (triangle->a.z + triangle->b.z + triangle->c.z) / 2);
+// 		ray->t = t;
 
-		//ray->color = triangle->color;
-		ray->shape_color = triangle->color;
-	}
-	return (1);
-}
+// 		//ray->color = triangle->color;
+// 		ray->shape_color = triangle->color;
+// 	}
+// 	return (1);
+// }
 
 int inter_triangles(t_ray *ray, t_triangle *triangle)
 {
